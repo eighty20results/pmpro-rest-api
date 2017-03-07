45ea972d09030061dc06197069636456f3d48754
Thomas Sjolshagen <thomas@eighty20results.com>
2017-03-06 23:36:21 -0700

    BUG/FIX: Endpoint fixed for Checking access to a post for a user of a certain level (hasaccess) BUG/FIX: Endpoint fixed for getting the membership level for a specific user ID (getmembershiplevel)

--- pmpro-rest-api.php
+++ pmpro-rest-api.php
@@ -3,10 +3,10 @@
 Plugin Name: Eighty/20 Results - REST API add-on for Paid Memberships Pro
 Plugin URI: https://eighty20results.com/wordpress-plugins/new-plugin-framework/
 Description: Adds REST API endpoints for Paid Memberships Pro
-Version: 1.0
+Version: 1.1
 Author: Eighty / 20 Results by Wicked Strong Chicks, LLC <thomas@eighty20results.com>
 Author URI: https://eighty20results.com/thomas-sjolshagen/
-Text Domain: e20rrapi
+Text Domain: pmpro-rest-api
 Domain Path: /languages
 License:
 
@@ -64,8 +64,50 @@ class pmproRestAPI extends WP_REST_Controller {
 
 		$this->option_name = strtolower( get_class( $this ) );
 
-		add_action( 'plugins_loaded', array( $this, 'addRoutes' ) );
-		add_action( 'http_api_curl', array( $this, 'force_tls_12' ) );
+		add_action( 'plugins_loaded', array( $this, 'addRoutes' ), 10 );
+		add_action( 'http_api_curl', array( $this, 'forceTLS12' ), 10 );
+
+		/* Commented out for now
+		add_action( 'init', array( $this, 'addRESTSupport' ), 25 );
+		add_filter( "rest_prepare_post", array( $this, 'checkAccessForRequest'), 10, 3 );
+		add_filter( "rest_prepare_page", array( $this, 'checkAccessForRequest'), 10, 3 );
+		*/
+	}
+
+	/**
+	 * Add CPTs to the REST support for PMPro
+	 */
+	public function addRESTSupport() {
+
+		global $wp_post_types;
+
+		$supported_posts = apply_filters( 'pmpro_restapi_supported_post_types', array( 'post', 'page' ) );
+
+		foreach( $supported_posts as $type ) {
+
+			if ( !in_array( $type, array( 'post', 'page' ) ) && isset( $wp_post_types[ $type ] ) ) {
+				$wp_post_types[$type]->show_in_rest = true;
+			}
+		}
+
+	}
+	/**
+	 * Trigger if/when the REST API is preparing the post/page/CPT for inclusion. (Not working).
+	 *
+	 * @param WP_REST_Response $response
+	 * @param WP_Post $post
+	 * @param WP_REST_Request $request
+	 *
+	 * @return WP_REST_Response
+	 */
+	public function checkAccessForRequest( $response, $post, $request ) {
+
+		error_log("Processing the checkAccessForRequest function: " . print_r( $response, true ));
+
+		if ( false == pmpro_has_membership_access( $post->ID ) ) {
+			$response = array( 'error' => "Access denied" );
+		}
+		return $response;
 	}
 
 	/**
@@ -75,74 +117,89 @@ class pmproRestAPI extends WP_REST_Controller {
 
 		// FIXME: Route definition for the levels array (if applicable)
 		add_action( 'rest_api_init', function () {
-			register_rest_route( 'pmpro/v1', '/hasaccess/post=(?P<post>\d+)/user=(?P<user>\d+)/levels=(?P<levels>\d+)', array(
-				'methods'  => WP_REST_Server::READABLE,
-				'callback' => array( $this, 'checkAccess' ),
-				'args'     => array(
-					'post'   => array(
-						'validate_callback' => function ( $param, $request, $key ) {
-							return is_numeric( $param );
-						}
+			register_rest_route(
+				'pmpro/v1',
+				'hasaccess/post=(?P<post>\d+)/user=(?P<user>\d+)/levels=(?P<levels>\d+)',
+				array(
+					'methods'  => WP_REST_Server::READABLE,
+					'callback' => array( $this, 'checkAccess' ),
+					'args'     => array(
+						'post'   => array(
+							'validate_callback' => function ( $param, $request, $key ) {
+								return is_numeric( $param );
+							}
+						),
+						'user'   => array(
+							'validate_callback' => function ( $param, $request, $key ) {
+
+
+								return ( empty( $param ) || is_numeric( $param ) );
+							}
+						),
+						'levels' => array(
+							'validate_callback' => function ( $param, $request, $key ) {
+
+								// FIXME: Validation callback for the levels array (not an array atm)
+
+								// Check that all of the values supplied are numbers
+								if ( is_array( $param )) {
+									$level_ints = array_map( 'is_numeric', $param );
+								} else {
+									$level_ints = array( is_numeric( $param ) );
+								}
+
+								// Return true if there are no instances of 'false' in the is_numeric check.
+								return ( empty( $param ) || ! in_array( false, $level_ints ) );
+							}
+						)
 					),
-					'user'   => array(
-						'validate_callback' => function ( $param, $request, $key ) {
-
-
-							return ( empty( $param ) || is_numeric( $param ) );
-						}
-					),
-					'levels' => array(
-						'validate_callback' => function ( $param, $request, $key ) {
-
-							// FIXME: Validation callback for the levels array (not an array atm)
-
-							// Check that all of the values supplied are numbers
-							$level_ints = array_map( 'is_numeric', $param );
-
-							// Return true if there are no instances of 'false' in the is_numeric check.
-							return ( empty( $param ) || ! in_array( false, $level_ints ) );
-						}
-					)
-				),
-			) );
+				)
+			);
 		} );
 
 		add_action( 'rest_api_init', function () {
-			register_rest_route( 'pmpro/v1', '/getlevelforuser/(?P<user>[0-9a-zA-Z\-]+)', array(
-				'methods'  => WP_REST_Server::READABLE,
-				'callback' => array( $this, 'getLevelForUser' ),
-				'args'     => array(
-					'user' => array(
-						'validate_callback' => function ( $param, $request, $key ) {
-							return is_numeric( $param );
-						}
+			register_rest_route(
+				'pmpro/v1',
+				'getlevelforuser/user=(?P<user>\d+)',
+				array(
+					'methods'  => WP_REST_Server::READABLE,
+					'callback' => array( $this, 'getLevelForUser' ),
+					'args'     => array(
+						'user' => array(
+							'validate_callback' => function ( $param, $request, $key ) {
+								return is_numeric( $param );
+							}
+						),
 					),
-				),
-			) );
+				)
+			);
 		} );
 	}
 
 	/**
 	 * REST API endpoint handler for PMPro post/page access check
 	 *
-	 * @param $request
+	 * @param WP_REST_Request $request
 	 *
-	 * @return array|bool|mixed|void|WP_Error
+	 * @return array|bool|mixed|WP_Error
 	 */
 	public function checkAccess( $request ) {
 
-		global $current_user;
+		$user_id                  = $request['user'];    //user id passed in
 
-		$this->logged_in = is_user_logged_in();
-		$this->user      = $current_user;
+		if ( empty( $user_id ) ) {
+			return new WP_Error( 'pmpro_rest_access', __( 'Cannot validate access for unknown user ID', 'pmpro-rest-api' ) );;
+		}
 
-		if ( false === $this->logged_in || false === current_user_can( 'manage_options' ) ) {
+		$this->user      = get_user_by('ID', $user_id );
+		$this->logged_in = $this->user->exists();
 
-			return new WP_Error( 'pmpro_rest_access', __( 'User does not have access to the PMPro REST API', 'e20rrapi' ) );
+		if ( false === $this->logged_in ) {
+
+			return new WP_Error( 'pmpro_rest_access', __( 'User does not have access to the PMPro REST API', 'pmpro-rest-api' ) );
 		}
 
 		$post_id                  = $request['post'];    //post id to check
-		$user_id                  = $request['user'];    //optional user id passed in
 		$return_membership_levels = $request['levels'];    //option to also include an array of membership levels with access to the post
 
 		return pmpro_has_membership_access( $post_id, $user_id, $return_membership_levels );
@@ -152,23 +209,25 @@ class pmproRestAPI extends WP_REST_Controller {
 	 *
 	 * Return the PMPro Membership Level for the specified user ID
 	 *
-	 * @param $request
+	 * @param WP_REST_Request $request
 	 *
 	 * @return bool|WP_Error
 	 */
 	public function getLevelForUser( $request ) {
 
-		global $current_user;
+		$user_id                  = $request['user'];    //optional user id passed in
 
-		$this->logged_in = is_user_logged_in();
-		$this->user      = $current_user;
-
-		if ( false === $this->logged_in || false === current_user_can( 'manage_options' ) ) {
-
-			return new WP_Error( 'pmpro_rest_access', __( 'User does not have access to the PMPro REST API', 'e20rrapi' ) );
+		if ( empty( $user_id ) ) {
+			return new WP_Error( 'pmpro_rest_access', __( 'Cannot find membership level for unknown user ID', 'pmpro-rest-api' ) );;
 		}
 
-		$user_id = $request['user'];
+		$this->user      = get_user_by('ID', $user_id );
+		$this->logged_in = $this->user->exists();
+
+		if ( false === $this->logged_in ) {
+
+			return new WP_Error( 'pmpro_rest_access', __( 'User does not have access to the PMPro REST API', 'pmpro-rest-api' ) );
+		}
 
 		return pmpro_getMembershipLevelForUser( $user_id );
 	}
@@ -194,18 +253,18 @@ class pmproRestAPI extends WP_REST_Controller {
 	 */
 	public function loadTranslation() {
 
-		$locale = apply_filters( "plugin_locale", get_locale(), "e20rrapi" );
-		$mo     = "e20rrapi-{$locale}.mo";
+		$locale = apply_filters( "plugin_locale", get_locale(), "pmpro-rest-api" );
+		$mo     = "pmpro-rest-api-{$locale}.mo";
 
 		// Paths to local (plugin) and global (WP) language files
 		$local_mo  = plugin_dir_path( __FILE__ ) . "/languages/{$mo}";
-		$global_mo = WP_LANG_DIR . "/e20rrapi/{$mo}";
+		$global_mo = WP_LANG_DIR . "/pmpro-rest-api/{$mo}";
 
 		// Load global version first
-		load_textdomain( "e20rrapi", $global_mo );
+		load_textdomain( "pmpro-rest-api", $global_mo );
 
 		// Load local version second
-		load_textdomain( "e20rrapi", $local_mo );
+		load_textdomain( "pmpro-rest-api", $local_mo );
 	}
 
 	/**
@@ -213,7 +272,7 @@ class pmproRestAPI extends WP_REST_Controller {
 	 *
 	 * @param $handle - File handle for the pipe to the CURL process
 	 */
-	public function force_tls_12( $handle ) {
+	public function forceTLS12( $handle ) {
 
 		// set the CURL option to use.
 		curl_setopt( $handle, CURLOPT_SSLVERSION, 6 );
